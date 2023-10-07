d4217105af6520aefa13c6509827633163aa662b
Agum Pratama <pratamaagum15@gmail.com>
2023-10-07 13:27:28 +0700

    Initial commit

---
+++ app/src/main/java/com/example/agumlaper/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.agumlaper
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
